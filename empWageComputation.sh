433ff2259d5eb1676e5d6ee7bf2ebc153de4c81b
nitinkandikatla <nitinkandikatla99@gmail.com>
2020-12-24 09:34:02 +0530

    Added UC1

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,12 @@
 echo "Welcome to Employee"
+#!/bin/bash -x
+
+echo "Employee Wage Compuation Problem"
+isPresent=1
+randomCheck=$(( RANDOM%2 ))
+if [[ $randomCheck -eq $isPresent ]]
+then
+    echo "Employee is present"
+else
+     echo "Employee is not present"
+fi
